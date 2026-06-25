82d8744e64f8081b16cca58b7b339e732b2980f5
pokuahhnyarko-lang <pokuahhnyarko@gmail.com>
2026-06-25 18:12:20 +0000

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,10 @@
+//=====🏅 KINGSLEY XD👑====
+
+module.exports = {
+  BOT_NAME: 'KING TECH', // ur bot name
+  OWNER_NAME: 'KINGSLEY XD', //your name
+  OWNER_NUMBER: '233535502036', // add your phone number
+  SESSION_ID: '', // add your session ID
+  MODE: "public", // public or private
+  TIMEZONE: 'Africa/Accra' // ur timezone
+};
